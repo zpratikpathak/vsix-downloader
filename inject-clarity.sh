d9489591823029947e4519f70a0c2e4ed908d617
Pratik Pathak <pratikpathak200@gmail.com>
2026-04-16 20:54:41 +0530

    build: use script for clarity injection to bypass command length limits

---
+++ inject-clarity.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+CLARITY_CODE='<script type="text/javascript">(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "wbo268qbmb");</script>'
+sed -i "s|<!-- CLARITY_SCRIPT -->|$CLARITY_CODE|g" index.html
